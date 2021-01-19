b2d9a5296657e07b25bd2203ee67527e4f53dd36
Bernardo Rosa <bernardoorosa@gmail.com>
2021-01-19 20:44:05 -0300

    :sparkles: Added home component

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { GET_ITEMS } from "../queries";
+import { useQuery } from "@apollo/client";
+import Card from "../components/Card";
+
+export default function Home() {
+  const { loading, error, data } = useQuery(GET_ITEMS);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{JSON.stringify(error, null, 2)}</div>;
+  return (
+    <main>
+      <div className="flex justify-between">
+        <h2 className="font-bold text-lg px-4">Menu</h2>
+        <Link to={"/createMenuItem"}>
+          <button
+            className="relative bg-blue-500 text-white p-2 rounded
+          text-base font-bold overflow-visible"
+          >
+            Add menu item
+          </button>
+        </Link>
+      </div>
+
+      <div className="w-full h-full flex flex-wrap items-center justify-around">
+        {data.items.map(({ id, name, price, type, photo }, index) => {
+          return (
+            <Card
+              id={id}
+              name={name}
+              type={type}
+              key={index}
+              photo={photo}
+              price={price}
+            />
+          );
+        })}
+      </div>
+    </main>
+  );
+}

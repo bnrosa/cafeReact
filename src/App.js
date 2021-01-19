000b47e82357d4d1a46f7ea47f40db49ec754a18
Bernardo Rosa <bernardoorosa@gmail.com>
2021-01-19 20:41:39 -0300

    :sparkles: Added apollo provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateItem from "./pages/CreateItem";
+import EditItem from "./pages/EditItem";
+import Navbar from "./components/Navbar";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="bg-gray-100 w-full h-full p-4">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/editMenuItem/:id" exact component={EditItem} />
+          <Route path="/createMenuItem" exact component={CreateItem} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
 
-ReactDOM.render(
+import App from "./App";
+
+const client = new ApolloClient({
+  link: createUploadLink({
+    uri: "http://localhost:4000/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
+render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App className="font-sans" />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
